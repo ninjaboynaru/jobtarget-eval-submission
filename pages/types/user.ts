f317bc879986dcaa8d07a88bc7c54b4d13039ea3
Thiago Pereira <tiago755@protonmail.com>
2024-07-02 09:29:21 -0500

    Removed unused User Interface properties

--- pages/types/user.ts
+++ pages/types/user.ts
@@ -2,10 +2,5 @@
 export interface IUser {
 	id: number;
 	nickname: string;
-	gravatar_id: string;
-	github_profile: string | null;
-	twitter_profile: string | null;
-	contributions_count: number;
-	link: string;
 	pull_requests: { title: string; repo_name: string, created_at: string }[];
 }
